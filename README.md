ba3f2f0f81fc419e26ccb3a17e6e91187b0fe70f
iTNTPiston <zhaoyizhu2000@gmail.com>
2023-06-26 21:44:04 -0700

    initial server implementation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ws-clipboard
-A simple python websocket server that copies received messages into system clipboard
+A simple python websocket server that copies received messages into system clipboard.
+
+This uses [`pyperclip`](https://pypi.org/project/pyperclip/) under the hood, which should work out of box for Windows and MacOS. For Linux, you may need to install `xclip` or `xsel` depending on your distro.
+
+## Install requirements
+```bash
+pip install -r requirements.txt
+```
+
+## Start server
+```bash
+python server.py [PORT]
+```
+If `[PORT]` is ommited, the default port is 8881
---
+++ server.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import sys
+import pyperclip
+DEFAULT_PORT = 8881
+
+def run_server(port):
+    async def handle_message(websocket):
+        async for message in websocket:
+            message = message.strip().replace("\x00", "\n")
+            length = len(message)
+            print(f"Received message {length=}")
+            pyperclip.copy(message)
+    start_server = websockets.serve(handle_message, "0.0.0.0", port)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    print(f"Server started on port {port}")
+    try:
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        print("Server stopped.")
+
+if __name__ == "__main__":
+    port = DEFAULT_PORT
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    run_server(port)
